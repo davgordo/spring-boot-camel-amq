468218cadfc2f897e375dcc3dc697e36d9ba720a
David Gordon <davgordo@redhat.com>
2017-04-12 16:01:35 -0400

    register RESTful endpoint

--- src/main/java/io/fabric8/quickstarts/camel/amq/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/amq/Application.java
@@ -15,19 +15,33 @@
  */
 package io.fabric8.quickstarts.camel.amq;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
-/**
- * The Spring-boot main class.
- */
 @SpringBootApplication
-@ImportResource({"classpath:spring/amq.xml", "classpath:spring/camel-context.xml"})
-public class Application {
+@ImportResource({ "classpath:spring/camel-context.xml" })
+public class Application extends RouteBuilder {
 
+    // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+                new CamelHttpTransportServlet(), "/api/*");
+        servlet.setName("CamelServlet");
+        return servlet;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        // using camel-context.xml instead to define routes
+    }
 }
